9db54c87fe4018cb0251609bd952bb4b65495830
David Dikman <ddikman@gmail.com>
2024-10-08 16:10:05 +0200

    Updated readme with correct instruction about result

--- date-archive-nope.php
+++ date-archive-nope.php
@@ -18,18 +18,4 @@ function date_archive_nope_return_404()
     }
 }
 
-function add_nofollow_to_elementor_pagination($html) {
-    // Use a regular expression to find and modify pagination links
-    $pattern = '/<a\s[^>]*class=["\']page-numbers["\'][^>]*>/i';
-    return preg_replace_callback($pattern, function($matches) {
-        $link = $matches[0];
-        if (strpos($link, 'rel=') === false) {
-            $link = str_replace('>', ' rel="nofollow">', $link);
-        } else {
-            $link = preg_replace('/rel=(["\'])(.*?)(["\'])/', 'rel="nofollow $2"', $link);
-        }
-        return $link;
-    }, $html);
-}
-
 add_action('template_redirect', 'date_archive_nope_return_404');
